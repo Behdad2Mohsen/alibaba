f2bc31982b05f935a7619294fecd32fa0161e6a7
Mohsen Zare <mohsenzare99b@gmail.com>
2018-07-29 21:32:42 +0430

    Some models created and some statics added and template.html created

--- web/models.py
+++ web/models.py
@@ -28,3 +28,17 @@ class Flight(models.Model):
     arrival_time = models.DateTimeField()
     source = models.CharField(max_length=50)
     destination = models.CharField(max_length=50)
+
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    national_code = models.CharField(max_length=10)
+    sex = models.BooleanField()
+    date_of_birth = models.DateField()
+
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
