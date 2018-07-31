0af7e600c18351eefb70683b7cdb4df213f8678d
Mohsen Zare <mohsenzare99b@gmail.com>
2018-07-31 13:11:37 +0430

    User models renamed to Profile and changed

--- web/models.py
+++ web/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
 
 
 class Office(models.Model):
@@ -39,6 +38,6 @@ class Passenger(models.Model):
 
 
 class Ticket(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
     passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
